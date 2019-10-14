d3f0d927615f97ed0599b95831d51b1652f9002a
James Dolan <jamesdolan1989@gmail.com>
2019-10-14 04:03:49 -0400

    Add files via upload

--- Cheers.py
+++ Cheers.py
@@ -1,22 +1,39 @@
 import kivy
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.pagelayout import PageLayout
-from kivy.uix.progressbar import ProgressBar
-from kivy.properties import ObjectProperty
-from kivy.core.text import Label as CoreLabel
 from kivy.lang.builder import Builder
-from kivy.graphics import Color, Ellipse, Rectangle
-from kivy.clock import Clock
-import LoadIcon
+from kivy.uix.screenmanager import ScreenManager, Screen
+#from LoadIcon import *
 
 
 #Root widget of application
 class MainRoot(PageLayout):
+    """
+    The main root of the application, to contain a loading
+    icon on launch, a search class for local pubs or bars,
+    a favorite screen class containing the local drink specials
+    and events, location content such as direction map,
+    telephone, and webiste. Include a setting screen as well.
+    """
     pass
 
-#App class
+class HomeScreen(Screen):
+    """
+    Testing this to be the main widget, or to be a widget
+    that is it's own child class, of the MainRoot class.
+    """
+    pass
+
+class SettingsScreen(Screen):
+    """
+    Settings screen for ideas such as changing location radius,
+    themes, colors, and info about me and the application version.
+    """
+    pass
+
+
 class Cheers(App):
+
 
     def build(self):
          return MainRoot()
